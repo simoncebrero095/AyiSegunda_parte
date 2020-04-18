0fae0d99fcedfd14da91fb0a395ad262bc436f38
Simón Cebrero <62322984+simoncebrero095@users.noreply.github.com>
2020-04-17 23:39:22 -0300

    localStorage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LocalStorageComponent } from './Component/local-storage/local-storage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocalStorageComponent
   ],
   imports: [
     BrowserModule,
