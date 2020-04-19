b3ae06141228abeb30c1317525a67cac2813bde1
Simón Cebrero <62322984+simoncebrero095@users.noreply.github.com>
2020-04-19 01:40:51 -0300

    locaStrorage

--- src/app/component/localStorage/compStorage/task/task.component.ts
+++ src/app/component/localStorage/compStorage/task/task.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Input } from '@angular/core';
+import {Task} from '../../compModule/Task';
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
+  @Input() task: Task;
 
   constructor() { }
 
